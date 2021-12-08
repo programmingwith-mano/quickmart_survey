124ecf20d652622d0903670362b08cb69216d155
Manoj Prabhakar <programmingwithmano@gmail.com>
2021-12-08 09:50:45 +0530

    AppBar changes

--- src/main/webapp/src/components/FormUserDetails.js
+++ src/main/webapp/src/components/FormUserDetails.js
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import {Typography, Button} from '@material-ui/core';
+import ButtonAppBar from './ButtonAppBar';
 
 export class FormUserDetails extends Component {
   continue = e => {
@@ -16,50 +17,45 @@ export class FormUserDetails extends Component {
     return (
       <MuiThemeProvider>
         <>
-          {/* <Dialog open
-            fullWidth
-            maxWidth='sm'
-          > */}
-            <AppBar title="Enter User Details" />
-            <br/>
-            <Typography align="center" variant="h6" gutterBottom component="div">
-              Quick Mart - Survey
-            </Typography>
-            <TextField
+          <ButtonAppBar/>
+          <br/>
+          <Typography align="center" variant="h6" gutterBottom component="div">
+              Customer Details
+          </Typography>
+          <TextField
               placeholder="Enter Your First Name"
               label="First Name"
               onChange={handleChange('firstName')}
               defaultValue={values.firstName}
               margin="normal"
               fullWidth
-            />
-            <br />
-            <TextField
+          />
+          <br />
+          <TextField
               placeholder="Enter Your Last Name"
               label="Last Name"
               onChange={handleChange('lastName')}
               defaultValue={values.lastName}
               margin="normal"
               fullWidth
-            />
-            <br />
-            <TextField
+          />
+          <br />
+          <TextField
               placeholder="Enter Your District"
               label="District"
               onChange={handleChange('district')}
               defaultValue={values.district}
               margin="normal"
               fullWidth
-            />
-            <br /><br /><br /><br />
-            <Typography align='center'>
+          />
+          <br /><br /><br />
+          <Typography align='center'>
             <Button
               color="primary"
               variant="contained"
               onClick={this.continue}
             >Continue</Button>
-            </Typography>
-          {/* </Dialog> */}
+          </Typography>
         </>
       </MuiThemeProvider>
     );
--- src/main/webapp/src/components/ProductList.js
+++ src/main/webapp/src/components/ProductList.js
@@ -5,8 +5,7 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
+import SearchAppBar from './SearchAppBar';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -51,54 +50,48 @@ export default function ProductList() {
   return (
     <MuiThemeProvider>
       <>
-          {/* <Dialog
-            open
-            fullWidth
-            maxWidth='md'
-          > */}
-    <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 400 }}
-      
-    >
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
-      >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
-    </Box>
-    {/* </Dialog> */}
+      <SearchAppBar/>
+        <Box
+          sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 400 }}
+        >
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+          >
+            <Tab label="Item One" {...a11yProps(0)} />
+            <Tab label="Item Two" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+            <Tab label="Item Four" {...a11yProps(3)} />
+            <Tab label="Item Five" {...a11yProps(4)} />
+            <Tab label="Item Six" {...a11yProps(5)} />
+            <Tab label="Item Seven" {...a11yProps(6)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          Item Seven
+        </TabPanel>
+      </Box>
     </>
     </MuiThemeProvider>
   );
